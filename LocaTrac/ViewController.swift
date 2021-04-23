f0a01ebfc1d4dddede37f463afba0bcd60aa44b2
Dhruva9891 <dhruvabeti@gmail.com>
2021-04-23 11:54:13 +0530

    Initial Commit

---
+++ LocaTrac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocaTrac
+//
+//  Created by Raghuveer Beti on 23/04/21.
+//  Copyright © 2021 Dhruva Beti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
