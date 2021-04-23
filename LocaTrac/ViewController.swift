6bb2c4233f59e25f5cbdcc4a827ad0a8b4dca578
Dhruva9891 <dhruvabeti@gmail.com>
2021-04-23 14:50:10 +0530

    Initial commit

--- LocaTrac/ViewController.swift
+++ LocaTrac/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager:CLLocationManager!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        
+        let status = CLLocationManager.authorizationStatus()
+        
+        if status == .notDetermined || status == .denied || status == .authorizedWhenInUse {
+            locationManager.requestAlwaysAuthorization()
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading()
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.mapType = MKMapType(rawValue: 0)!
+        mapView.userTrackingMode = MKUserTrackingMode(rawValue: 2)!
+        
+    }
+    
+    // Mark LocationManager Delegate Methods
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        for location in locations {
+            print(location.coordinate.latitude)
+            print(location.coordinate.longitude)
+        }
     }
 
 
